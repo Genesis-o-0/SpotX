53efd2767b6506586268d84d3c12b22228179055
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-12-06 14:09:50 +0200

    Refactor code and organize the files structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,17 @@ import getSectionsdata from "@/utils/getSectionsdata";
 import AppSections from "./sections/AppSections";
 import Loading from "./_components/shared/Loading";
 import { getUiBuilders } from "@/utils/getUiBuilders";
-interface LooseObject {
-  [key: string]: any;
-}
 
 export default async function Home() {
   // getting configuration data from UI-Builders API
-  const configurationData = await getUiBuilders();
-  const sections = configurationData.data;
+  const uiBuildersConfig = await getUiBuilders();
+  const sections = uiBuildersConfig.data;
   const sectionsData = sections.map(async (section) => {
-    // this object will contain all the sections data with a key value of section title
-    const sectionsObj: LooseObject = {};
     const resolved = await getSectionsdata(section.content, section.filters);
-    sectionsObj[section.title] = resolved;
-    return sectionsObj;
+    return { [section.title]: resolved };
   });
 
-  const ResolvedSectionsData = await Promise.all(sectionsData).then((data) => {
+  const resolvedSectionsData = await Promise.all(sectionsData).then((data) => {
     return data;
   });
 
@@ -28,7 +22,7 @@ export default async function Home() {
     <main>
       <SearchBar />
       <Suspense fallback={<Loading />}>
-        {ResolvedSectionsData.map((section, index) => {
+        {resolvedSectionsData.map((section, index) => {
           return <AppSections key={index} data={section} />;
         })}
       </Suspense>
--- app/sections/AppSections.tsx
+++ app/sections/AppSections.tsx
@@ -1,50 +1,36 @@
 import { Container } from "@mui/material";
 import Units from "./UnitsSections";
 import RegionSection from "./RegionsSections";
+import { containerStyles } from "./sharedStyles/sharedStyles";
 
 const AppSections: React.FC<{ data: any }> = async ({ data }) => {
   return (
-    <Container
-      style={{
-        margin: "auto",
-        padding: 0,
-        marginTop: "20px",
-        width: "90%",
-      }}
-    >
+    <Container style={containerStyles}>
       {data["North Coast"] && (
         <RegionSection
-          northCoastData={data["North Coast"]["data"]["sub_regions"]}
+          regionData={data["North Coast"]["data"]["sub_regions"]}
           title={"Check our units in North Coast"}
         />
       )}
       {data["Most Popular"] && (
         <RegionSection
-          northCoastData={data["Most Popular"]?.data}
+          regionData={data["Most Popular"]?.data}
           title={"Most Popular Regions"}
         />
       )}
 
       {data["Least Price Units"] && (
-        <>
-          <h3
-            style={{ color: "white", fontSize: "1.5rem", marginBottom: "15px" }}
-          >
-            Least Price Units
-          </h3>
-          <Units unitsData={data["Least Price Units"]?.data} />
-        </>
+        <Units
+          unitsData={data["Least Price Units"]?.data}
+          title={"Least Price Units"}
+        />
       )}
 
       {data["units accepts 5 guests"] && (
-        <>
-          <h3
-            style={{ color: "white", fontSize: "1.5rem", marginBottom: "15px" }}
-          >
-            Units that accept 5 guests
-          </h3>
-          <Units unitsData={data["units accepts 5 guests"]?.data} />
-        </>
+        <Units
+          unitsData={data["units accepts 5 guests"]?.data}
+          title={"Units that accept 5 guests"}
+        />
       )}
     </Container>
   );
